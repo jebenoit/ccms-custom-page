3f8b83933aff79ebcf6917ea2f27c396d57d5bea
Harry Moore <harrymoore@yahoo.com>
2017-10-25 13:46:14 -0400

    initial

---
+++ myblogs/gadgets/my-blogs-list/my-blogs-list.js
@@ -0,0 +1,110 @@
+define(function(require, exports, module) {
+
+    require("css!./my-blogs-list.css");
+    var html = require("text!./my-blogs-list.html");
+
+    var Empty = require("ratchet/dynamic/empty");
+
+    var UI = require("ui");
+
+    return UI.registerGadget("my-blogs-list", Empty.extend({
+
+        TEMPLATE: html,
+
+        /**
+         * Binds this gadget to the /myblogs route
+         */
+        setup: function() {
+            this.get("/projects/{projectId}/myblogs", this.index);
+        },
+
+        /**
+         * Puts variables into the model for rendering within our template.
+         * Once we've finished setting up the model, we must fire callback().
+         *
+         * @param el
+         * @param model
+         * @param callback
+         */
+        prepareModel: function(el, model, callback) {
+
+            // get the current project
+            var project = this.observable("project").get();
+
+            // the current branch
+            var branch = this.observable("branch").get();
+
+            // call into base method and then set up the model
+            this.base(el, model, function() {
+
+                // query for blog post instances
+                branch.queryNodes({ "_type": "mmcx:blogpost" }).then(function() {
+
+                    // store blog post node on the model (as a list) and then fire callback
+                    model.nodes = this.asArray();
+
+                    // add "imageUrl" attribute
+                    // add "browseUrl" attribute
+                    for (var i = 0; i < model.nodes.length; i++)
+                    {
+                        var node = model.nodes[i];
+
+                        node.imageUrl256 = "/preview/repository/" + node.getRepositoryId() + "/branch/" + node.getBranchId() + "/node/" + node.getId() + "/default?size=256&name=preview256&force=true";
+                        node.imageUrl128 = "/preview/repository/" + node.getRepositoryId() + "/branch/" + node.getBranchId() + "/node/" + node.getId() + "/default?size=128&name=preview128&force=true";
+                        node.browseUrl = "/#/projects/" + project._doc + "/documents/" + node._doc;
+                    }
+
+                    callback();
+                });
+            });
+        },
+
+        /**
+         * This method gets called before the rendered DOM element is injected into the page.
+         *
+         * @param el the dom element
+         * @param model the model used to render the template
+         * @param callback
+         */
+        /*
+        beforeSwap: function(el, model, callback)
+        {
+            this.base(el, model, function() {
+                callback();
+            });
+        },
+        */
+
+        /**
+         * This method gets called after the rendered DOM element has been injected into the page.
+         *
+         * @param el the new dom element (in page)
+         * @param model the model used to render the template
+         * @param originalContext the dispatch context used to inject
+         * @param callback
+         */
+        afterSwap: function(el, model, originalContext, callback)
+        {
+            this.base(el, model, originalContext, function() {
+
+                // find all .media-popups and attach to a lightbox
+                $(el).find(".media-popup").click(function(e) {
+
+                    e.preventDefault();
+
+                    var nodeIndex = $(this).attr("data-media-index");
+                    var node = model.nodes[nodeIndex];
+
+                    UI.showPopupModal({
+                        "title": "Viewing: " + node.title,
+                        "body": "<div style='text-align:center'><img src='" + node.imageUrl + "'></div>"
+                    });
+                });
+
+                callback();
+            });
+        }
+
+    }));
+
+});
---
+++ myblogs/index.js
@@ -0,0 +1,9 @@
+define(function(require) {
+
+    // page: "my-blogs-list"
+    require("./gadgets/my-blogs-list/my-blogs-list.js");
+
+    // global CSS overrides
+    require("css!./styles/my-blogs-list.css");
+
+});
